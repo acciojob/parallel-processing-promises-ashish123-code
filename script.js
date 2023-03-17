88b074cc5c52e500bbbe2fc57c7f54fd4e3daee7
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-03-17 20:43:21 +0530

    vh

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const images = [
+    { url: "https://picsum.photos/200/300" },
+    { url: "https://picsum.photos/200/300" },
+    { url: "https://picsum.photos/200/300" },
+    { url: "https://picsum.photos/200/300" },
+    { url: "https://picsum.photos/200/300" }
+  ];
+
+  const downloadImages = async () => {
+    const promises = images.map(image => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = image.url;
+        img.onload = () => {
+          resolve(img);
+        };
+        img.onerror = () => {
+          reject(`Failed to load image's URL: ${image.url}`);
+        };
+      });
+    });
+
+    try {
+      const result = await Promise.all(promises);
+      result.forEach(img => {
+        const output = document.getElementById("output");
+        output.appendChild(img);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const button = document.getElementById("download-images-button");
+  button.addEventListener("click", downloadImages);
