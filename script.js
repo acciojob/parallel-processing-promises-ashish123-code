a667565dab0cf3ab6bf239deb666d419ceffe5d1
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-03-17 21:04:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const images = [
     { url: "https://picsum.photos/id/238/200/300" },
     { url: "https://picsum.photos/id/237/200/300" },
-    { url: "https://picsum.photos/id/238/200/300" }
+    { url: "https://picsum.photos/id/237/200/300" }
   ];
 
   const downloadImages = async () => {
