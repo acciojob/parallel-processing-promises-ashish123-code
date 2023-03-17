5297598eb2708227af102571d4e039fa7f6d9404
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-03-17 20:49:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const images = [
-    { url: "https://picsum.photos/200/300" },
-    { url: "https://picsum.photos/200/300" },
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/237/200/300" },
     { url: "https://picsum.photos/id/237/200/300" }
   ];
 
